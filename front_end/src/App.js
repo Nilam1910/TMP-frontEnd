85f7fc2d537d8ed85c6688185d0c7f404273a9af
kaywish <kaywishawaan@gmail.com>
2022-09-25 16:26:32 -0400

    Added the popup and changed import material/icons

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React, { Component } from 'react'
 import {useState} from 'react';
-import Map, {Marker} from 'react-map-gl';
-import Room from '@mui/icons-material/Room';
+import Map, {Marker, Popup} from 'react-map-gl';
+import {Room, Star }from '@material-ui/icons';
+// Changed how we imported the icons
+
 
 let baseURL = ""
 if(process.env.NODE_ENV === "development"){
@@ -21,6 +23,7 @@ function App() {
     zoom: 4
   })
   return (
+   
     <Map
         initialViewState={{ ...viewport }}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
@@ -29,6 +32,7 @@ function App() {
         mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
       >
+
       <Marker
       longitude={-97.4}
       latitude={38}
@@ -37,12 +41,43 @@ function App() {
       >
       <Room style={{fontSize:viewport.zoom * 10}}/>
      </Marker>
+
+
+     <Popup 
+     longitude={-97.4} 
+     latitude={38}
+     closeButton={true}
+     closeOnClick={false}
+     anchor="left"  
+     >  
+      <div className ="card">
+        <label> Place </label>
+        <h4 className="place"> Manhattan </h4>
+        <label> Review </label>
+        <p className="desc"> I love central park!</p>
+        <label> Rating </label>
+        <div className="stars">
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        </div>
+        <label> Information</label>
+        <span className="username"> Created by <b> Tony </b></span>
+        <span className="date"> 1 hour ago </span>
+        
+      </div>
+      </Popup>
+
+
       </Map>
   );
 }
 
 // comment //
 // some thing testig
+// I added the Popup 
 
 
 export default App
