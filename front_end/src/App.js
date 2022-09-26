bd8097ddebd8f87007f4b390a222a765b701c88a
jbep <joshuabemartin@gmail.com>
2022-09-25 22:29:43 -0400

    fixed up

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -15,69 +15,73 @@ if(process.env.NODE_ENV === "development"){
 console.log("Current base URL: ", baseURL)
 
 function App() {
+  const [showPopup, setShowPopup] = React.useState(true);
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: "100%",
+    height: "100%",
     longitude: -97.92,
     latitude: 39.38,
     zoom: 4
-  })
+  });
+
   return (
-   
-    <Map
-        initialViewState={{ ...viewport }}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        transitionDuration="200"
-        style={{ width: "100vw", height: "100vh" }}
-        mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      >
+    <div style={{ height: "100vh", width: "100%"}}>
+      <Map
+          initialViewState={{ ...viewport }}
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+          width="100%"
+          height="100%"
+          transitionDuration="200"
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          onViewportChange={(Viewport) => setViewport(viewport)}
+        >
+        <>
+          <Marker
+          longitude={-97.4}
+          latitude={38}
+          offsetLeft={-3.5 * viewport.zoom}
+          offsetTop={-10 * viewport.zoom}
+          >
+          <Room style={{fontSize:viewport.zoom * 10, color: "slateblue"}}/>
+         </Marker>
 
-      <Marker
-      longitude={-97.4}
-      latitude={38}
-      offsetLeft={-3.5 * viewport.zoom}
-      offsetTop={-10 * viewport.zoom}
-      >
-      <Room style={{fontSize:viewport.zoom * 10}}/>
-     </Marker>
+        {showPopup && (
+         <Popup
+         longitude={-97.4}
+         latitude={38}
+         closeButton={true}
+         closeOnClick={false}
+         anchor="top"
+         >
+          <div className ="card">
+            <label> Place </label>
+            <h4 className="place"> Manhattan </h4>
+            <label> Review </label>
+            <p className="desc"> I love central park!</p>
+            <label> Rating </label>
+            <div className="stars">
+            <Star className="star" />
+            <Star className="star" />
+            <Star className="star" />
+            <Star className="star" />
+            <Star className="star" />
+            </div>
+            <label> Information</label>
+            <span className="username"> Created by <b> Tony </b></span>
+            <span className="date"> 1 hour ago </span>
 
+          </div>
+          </Popup>)}
+        </>
 
-     <Popup 
-     longitude={-97.4} 
-     latitude={38}
-     closeButton={true}
-     closeOnClick={false}
-     anchor="left"  
-     >  
-      <div className ="card">
-        <label> Place </label>
-        <h4 className="place"> Manhattan </h4>
-        <label> Review </label>
-        <p className="desc"> I love central park!</p>
-        <label> Rating </label>
-        <div className="stars">
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-        </div>
-        <label> Information</label>
-        <span className="username"> Created by <b> Tony </b></span>
-        <span className="date"> 1 hour ago </span>
-        
+        </Map>
       </div>
-      </Popup>
-
-
-      </Map>
   );
 }
 
 // comment //
 // some thing testig
-// I added the Popup 
+// I added the Popup
 
 
 export default App
