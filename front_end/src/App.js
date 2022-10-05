1bc1230918d6bc2b245220195db3f4a32756f7e6
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-10-05 01:51:10 -0500

    fixed css and arrange codes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import Map, {Marker, Popup} from 'react-map-gl';
 import {Room, Star }from '@material-ui/icons';
 import { format } from 'timeago.js';
 import Login from "./components/Login"
-
+import Register from "./components/Register"
+import NewForm from "./components/NewForm"
 
 let baseURL = ""
 
@@ -16,8 +17,8 @@ if(process.env.NODE_ENV === "development"){
 console.log("Current base URL: ", baseURL)
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(){
+    super()
       this.state = {
         pins: [],
         viewport: {
@@ -32,68 +33,95 @@ class App extends Component {
         showLogin: false,
         setCurrentUser: null ,
         showLogout: false,
-
+        showRegister: false,
+        showForm: false
       }
   }
   // componentDidMount - runs only once when the comp is mounted for the first time
-  componentDidMount() {
-    this.getPins()
-    
-  }
-
-  getPins = () => {
-    fetch(baseURL + '/pins')
-      .then(res => {
-        if(res.status === 200) {
-          return res.json()
-        } else {
-          return []
-        }
-      }).then(data => {
-        console.log('data', data)
-        this.setState({pins: data.pins})
-      })
-  }
-
-  handleViewportChange = viewport => {
-    this.setState({
-      viewport: { ...this.state.viewport, ...viewport }
-    })
-  }
-
-
-  // handlePopupThree = (id) => {
-  //   currentlocation(id)
-  // }
-
-  handlePopUp = (id) => {
-    console.log("handle popup triggered")
-    this.setState({
-      currentLocation: id
-    })
-  }
-
-  handlePopUpTwo = () => {
-    console.log("handle popup triggered")
-    this.setState({
-      currentLocation: null
-    })
-  }
-
-  showLoginPopup = () => {
-    console.log("login popup triggered")
-    this.setState({
-      showLogin: true
-    })
-  }
-
-  closeLoginPopup = () => {
-    console.log("login popup closed")
-    this.setState({
-      showLogin: false
+    componentDidMount() {
+      this.getPins()
+    }
       
+    getPins = () => {
+      fetch(baseURL + '/pins')
+        .then(res => {
+          if(res.status === 200) {
+            return res.json()
+          } else {
+            return []
+          }
+        }).then(data => {
+          console.log('data', data)
+          this.setState({pins: data.pins})
+        })
+    }
+
+    handleViewportChange = viewport => {
+      this.setState({
+        viewport: { ...this.state.viewport, ...viewport }
+      })
+    }
+
+    handlePopUp = (id) => {
+      console.log("handle popup triggered")
+      this.setState({
+        currentLocation: id
+      })
+    }
+
+    handlePopUpTwo = () => {
+      console.log("handle popup triggered")
+      this.setState({
+        currentLocation: null
+      })
+    }
+    showRegisterPopup = () => {
+      console.log("register popup triggered")
+      this.setState({
+        showRegister: true
+      })
+    }
+
+    closeRegisterPopup = () => {
+      console.log("register popup closed")
+      this.setState({
+        showRegister: false
+      })
+    }
+
+    handleRegister = (e) => {
+    e.preventDefault()
+    console.log("etarget", e.target)
+    fetch(baseURL + '/users/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: e.target.username.value,
+        email: e.target.email.value,
+        password: e.target.password.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(resJson => {
+      console.log(resJson)
+      this.getPins()
     })
   }
+    showLoginPopup = () => {
+      console.log("login popup triggered")
+      this.setState({
+        showLogin: true
+      })
+    }
+
+    closeLoginPopup = () => {
+      console.log("login popup closed")
+      this.setState({
+        showLogin: false
+        
+      })
+    }
 
   handleLogin = (e) => {
     e.preventDefault()
@@ -125,48 +153,60 @@ class App extends Component {
     })
   }
 
-  showLogoutButton = () => {
-    console.log("shows logout button")
-    this.setState({
-      setCurrentUser: true,
-      showLogout: true
-    })
+    showLogoutButton = () => {
+      console.log("shows logout button")
+      this.setState({
+        setCurrentUser: true,
+        showLogout: true
+      })
+    }
+
+    handleLogOut= () => {
+      this.setState({
+        showLogout: true,
+        setCurrentUser: false,
+        showLogin: false,
+      })
+    }
+
+    handleAddForm = (pin) => {
+      console.log("handleAddFormWorking")
+      const copyPins = [...this.state.pins]
+      copyPins.unshift(pin)
+      this.setState({pins: copyPins})
+    }
+    
+    deletePin = (id) => {
+    fetch(baseURL + '/pins/' + id, {
+    method: 'DELETE'
+    }).then( res => {
+      const copyPins = [...this.state.pins]
+      const findIndex = this.state.pins.findIndex(pin => pin._id === id)
+      copyPins.splice(findIndex, 1)
+      this.setState({
+        pins: copyPins
+      });
+    });
   }
 
-
-   handleLogOut= () => {
-     this.setState({
-       showLogout: true,
-    setCurrentUser: false,
-    showLogin: false,
-  
-     }
-     )
-     
+    showFormPopup = () => {
+      console.log("form popup triggered")
+      this.setState({
+        showForm: true
+      })
     }
-    //  fetch(baseURL + "/users/signout"), {
-    //    method: "DELETE",
-    //  } .then ((response) => response.json())
-    //  .then ((data) => {
-    //    console.log(data)
-    //  })  
 
-   
-
-
-  
-
-
-
-
-  
-
-
+    closeFormPopup = () => {
+      console.log("form popup closed")
+      this.setState({
+        showForm: false
+      })
+    }
   render(){
     const { viewport } = this.state;
     return (
-      <div style={{ height: "100vh", width: "100vw"}}>
-        <Map
+    <div style={{ height: "100vh", width: "100vw"}}>
+      <Map
             initialViewState={{ ...viewport }}
             mapboxAccessToken={process.env.REACT_APP_MAPBOX}
             width="100%"
@@ -174,84 +214,94 @@ class App extends Component {
             transitionDuration="200"
             mapStyle="mapbox://styles/mapbox/streets-v9"
             onViewportChange={() => this.handleViewportChange()}
-          >
+      >
           {this.state.pins.map((pins, index) => {
             // console.log(pins)
             return (
-              <div key={pins._id}>
-                <Marker
-                  longitude={pins.longitude}
-                  latitude={pins.latitude}
-                  offsetLeft={-viewport.zoom * 5}
-                  offsetTop={-viewport.zoom * 10}
-                  onClick={() => this.handlePopUp(pins._id)}
-                >
-                </Marker>
-                // {console.log("showpopup", this.state.showPopup)}
-                {pins._id === this.state.currentLocation && (
-                <Popup
-                 longitude={pins.longitude}
-                 latitude={pins.latitude}
-                 closeButton={true}
-                 closeOnClick={false}
-                 onClose={() => this.handlePopUpTwo()}
-                 anchor="left"
-                 >
-                  <div className ="card">
-                    <label> Place </label>
-                    <h4 className="place"> {pins.title} </h4>
-                    <label> Review </label>
-                    <p className="desc"> {pins.description}</p>
-                    <label> Rating </label>
-                    <div className="stars">
-                    {Array(pins.rating).fill(<Star className="star" />)}
-                 
-                    </div>
-                    <label> Information</label>
-                    <span className="username"> Created by <b> {pins.username}</b></span>
-                    <span className="date"> {format(pins.createdAt)} </span>
-                  </div>
-                </Popup>
-              )}
-              </div>
+        <div  key={pins._id}>
+            <Marker
+              longitude={pins.longitude}
+              latitude={pins.latitude}
+              offsetLeft={-viewport.zoom * 5}
+              offsetTop={-viewport.zoom * 10}
+              onClick={() => this.handlePopUp(pins._id, pins.latitude, pins.longitude)}
+            >
+            </Marker>
+                  {/*  {console.log("showpopup", this.state.showPopup)} */}
+            {pins._id === this.state.currentLocation && (
+            <Popup
+              longitude={pins.longitude}
+              latitude={pins.latitude}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => this.handlePopUpTwo()}
+              anchor="left"
+            >
+          <div className ="card">
+              <label> Place </label>
+              <h4 className="place"> {pins.title} </h4>
+              <label> Review </label>
+              <p className="desc"> {pins.description}</p>
+              <label> Rating </label>
+            <div className="stars">
+                {Array(pins.rating).fill(<Star className="star" />)}
+            </div>
+              <label> Information</label>
+                <span className="username"> Created by: <b> {pins.username} </b> </span>
+                <span className="date"> Created When: <b> {format(pins.createdAt)} </b> </span>
+              <button className="buttonDelete"
+                onClick={() => this.deletePin(pins._id)}
+              > Delete Pin 
+              </button>
+          </div>
+            </Popup>
+          )}
+        </div>
         )
       })}
-         {this.setCurrentUser ? (
-         <button className="button logout" onClick={this.handleLogOut}> 
-          Log out
-          </button> ) : (
-          <div className="buttons">
-          <button className="button login" onClick={this.showLoginPopup}>
-          Log in
-          </button>
-          <button className="button register">
-          Register
-          </button>
-          </div>
-          )}
+      <div className="buttons">
+         
+           <button  className="button register" onClick={this.showRegisterPopup}> Register </button>
+          {this.state.showRegister && (
+            <Register
+            closeRegisterPopup={this.closeRegisterPopup}
+            getPins={this.getPins}
+            handleRegister={this.handleRegister}
+            />
+            )}
+
           {this.state.showLogin && (
-          <Login
-          closeLoginPopup={this.closeLoginPopup}
-          getPins={this.getPins}
-          handleLogin={this.handleLogin}
-          handleLogOut={this.handleLogOut}
-          
+            <Login
+            closeLoginPopup={this.closeLoginPopup}
+            getPins={this.getPins}
+            handleLogin={this.handleLogin}
+            handleLogOut={this.handleLogOut}
+            />
+            )} 
 
+          {this.setCurrentUser ? (
+              <button className="loginButton" onClick={this.showLoginPopup}>Log in</button>
+              ) : ( 
+              <button className="logoutButton" onClick={this.handleLogOut}>Log out</button> 
+                  
+            )}               
+
+           
+           <button className="addPinButton"
+                  onClick={this.showFormPopup}
+          > Add Pin 
+          </button>
+          {this.state.showForm && (
+          <NewForm
+          handleAddForm={this.handleAddForm}
+          closeFormPopup={this.closeFormPopup}
           />
-        )} 
-
-
-
-
-          </Map>
-        </div>
+        )}
+      </div>
+      </Map>
+    </div>
     );
   }
 }
-
-// comment //
-// some thing testig
-// I added the Popup
-
 
 export default App
--- front_end/src/components/Login.js
+++ front_end/src/components/Login.js
@@ -1,56 +1,53 @@
-import React, { Component, useRef } from 'react'
-import {Cancel , Room, TurnedIn} from "@material-ui/icons"
+import React, { Component } from 'react'
+import {Cancel , Room} from "@material-ui/icons"
 import "./login.css"
 
-let baseURL = ""
+  let baseURL = ""
 
-if(process.env.NODE_ENV === "development"){
-  baseURL = "http://localhost:3001"
-} else {
-  baseURL = "Your heroku backend url here"
-}
-console.log("Current base URL: ", baseURL)
-
- class Login extends Component {
-  constructor(props){
-      super(props)
-      this.state = {
-        success:false,
-        error: false,
-        setError: false,
-        setSuccess: true,
+    if(process.env.NODE_ENV === "development"){
+    baseURL = "http://localhost:3001"
+    } else {
+    baseURL = "Your heroku backend url here"
     }
-}
-      
+    console.log("Current base URL: ", baseURL)
 
-render() {
-
-  return (
-
-<div className = "loginContainer">
-<div className = "logo">
-<Room />
-    Travel Pins 
-</div>
-<h1 className="h1-login">Log In</h1>
-        <form onSubmit={this.props.handleLogin}>
-          <label className="label1" htmlFor="name">Username: </label>
-          <input id="username" name="username" className="username" type="text" placeholder="username" />
-          <label className="label1" htmlFor="name">Email: </label>
-          <input  id="email" name="email" className="email" type="email" placeholder="email" />
-          <label className="password" htmlFor="name">Password: </label>
-          <input id="password" name="password" className="password" type="password" placeholder="password" />
-          <input className="loginButton" type="submit" value="Login" />
-        </form>
-        <Cancel
-        className="loginCancel"
-        onClick={this.props.closeLoginPopup}
-        />
-</div>
+  class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          success:false,
+          error: false,
+          setError: false,
+          setSuccess: true,
+      }
+    }
+    
+  render() {
+    return (
+      <div className = "loginContainer">
+        <div className = "logo">
+        <Room />
+            Travel Pins 
+        </div>
+        <h1 className="h1-login">Log In</h1>
+          <form onSubmit={this.props.handleLogin}>
+            <label className="label1" htmlFor="name">Username: </label>
+              <input id="username" name="username" className="username" type="text" placeholder="username" />
+            <label className="label1" htmlFor="name">Email: </label>
+              <input id="email" name="email" className="email" type="email" placeholder="email" />
+            <label className="password" htmlFor="name">Password: </label>
+              <input id="password" name="password" className="password" type="password" placeholder="password" />
+              <input className="loginButton" type="submit" value="Login" />
+          </form>
+            <Cancel
+            className="loginCancel"
+            onClick={this.props.closeLoginPopup}
+            />
+      </div>
 
 
     )
-}
+  }
 }
 
 
