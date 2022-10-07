ced7fc19a18f7620d08e4597a042b490a70c5de7
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-10-07 14:10:36 -0500

    deleted note use codes for heroku depolyment

--- front_end/src/Homepage.js
+++ front_end/src/Homepage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import {  Link } from "react-router-dom"
 import Register from './components/Register'
-// import Login from "./components/Login"
 import {  BiXCircle } from "react-icons/bi";
 
 import "./homePage.css"
@@ -55,72 +54,6 @@ class Homepage extends Component {
     })
   }
 
-  // handleRegister = (e) => {
-  //   e.preventDefault()
-  //   console.log("eTarget", e.target)
-  //   fetch(baseURL + '/users/register', {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       username: e.target.username.value,
-  //       email: e.target.email.value,
-  //       password: e.target.password.value
-  //     }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then(res => res.json())
-  //   .then(resJson => {
-  //     console.log(resJson)
-  //     this.getPins()
-  //   })
-  // }
-
-  // handleLogin = (e) => {
-  //   e.preventDefault()
-  //   console.log("eTarget", e.target.username.value, e.target.email.value, e.target.password.value)
-  //   console.log(baseURL)
-  //   fetch(baseURL + '/users/signin', {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       username: e.target.username.value,
-  //       email: e.target.email.value,
-  //       password: e.target.password.value
-  //     }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then(res => {
-  //     if (res.ok) return res.json()
-  //     console.log(res)
-  //   })
-  //     .then(resJson => {
-  //     console.log("resjson", resJson)
-  //     this.getPins()
-      
-  //   })
-   
-  // }
-
-
-  // getPins = () => {
-	// 	fetch(baseURL + '/pins')
-	// 		.then(res => {
-	// 			if(res.status === 200) {
-	// 				return res.json()
-	// 			} else {
-	// 				return []
-	// 			}
-	// 		}).then(data => {
-	// 			console.log('data', data)
-	// 			this.setState({pins: data.pins})
-	// 		})
-	// }
-
-  // handleViewportChange = viewport => {
-  //   this.setState({
-  //     viewport: { ...this.state.viewport, ...viewport }
-  //   })
-  // }
 
   render() {
     return (
@@ -137,13 +70,7 @@ class Homepage extends Component {
         />
       )}
     
-      {/* {this.state.showLogin && (
-        <Login
-          closeLoginPopup={this.closeLoginPopup}
-          getPins={this.getPins}
-          handleLogin={this.handleLogin}
-        />
-      )} */}
+     
        <button className="button register" onClick={this.showRegisterPopup}>
         Register
       </button>
